3af8a41b34644c3248c4a69d505990c9cba1a51f
kirkb1998 <kirkb1998@gmail.com>
2021-01-07 18:47:46 -0700

    more noodling with different approximations

--- primeplot.py
+++ primeplot.py
@@ -18,19 +18,32 @@ def pi(n):
             if sp.isprime(x):
                 out+=1
         return out
-
+def digappx(n):
+    n = np.floor(n).astype(int)
+    if isinstance(n,(list,np.ndarray)):
+        out = []
+        for cur_num in n:
+            out.append(cur_num / (2*len(str(cur_num))))
+        return out
+    else:
+        out=n / len(str(n))
+        return out
+    
 # Data for plotting
 t = np.arange(5, 100000, 0.1)
-s = pi(t) / np.floor(t).astype(int)
+s = pi(t) #/ np.floor(t).astype(int)
 v = 1 / np.log(t)
+w = digappx(t)
+u = t / np.log(t)
 
 fig, ax = plt.subplots()
-ax.plot(t, s)
-ax.plot(t,v)
-
+ax.plot(t,s, color='green')
+ax.plot(t,u, color='orange')
+ax.plot(t,w, color='blue')
 ax.set(xlabel='x', ylabel='pi(x)/x',
-       title='About as simple as it gets, folks')
+       title='Primeywimey')
 ax.grid()
 
 fig.savefig("test.png")
 plt.show()
+#print(w)
